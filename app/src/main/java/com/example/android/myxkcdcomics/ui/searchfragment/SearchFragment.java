4002d8122c7f671e10ea5cf4a1183b1295df7dd5
fireflyfif <fif.iva@gmail.com>
2018-10-15 15:46:51 +0200

    configure layouts to support tablets

--- app/src/main/java/com/example/android/myxkcdcomics/ui/searchfragment/SearchFragment.java
+++ app/src/main/java/com/example/android/myxkcdcomics/ui/searchfragment/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.myxkcdcomics.ui.searchfragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,9 +11,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.android.myxkcdcomics.R;
 
@@ -25,6 +29,8 @@ public class SearchFragment extends Fragment {
 
     @BindView(R.id.search_webview)
     WebView webView;
+    @BindView(R.id.web_progress_bar)
+    ProgressBar progressBar;
 
     @Nullable
     @Override
@@ -34,41 +40,60 @@ public class SearchFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
         ButterKnife.bind(this, rootView);
 
-        webView.setWebViewClient(new InnerWebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (Uri.parse(url).getHost().equals("https://relevantxkcd.appspot.com/")) {
+                    return false;
+                }
+
+
+                Log.d(TAG, "Taken url from web: " + url);
+                // Show the loaded url into the web view
+                webView.loadUrl(url);
+
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+
+                // TODO: Load only a specific element from the web page
+            /*webView.loadUrl("javascript:(function() { " +
+                    "document.getElementsByTagName('comic')[0].style.display='none';" +
+                    "})()");*/
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
         webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setSupportZoom(true);
         webView.getSettings().setUseWideViewPort(true);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
         webView.loadUrl("https://relevantxkcd.appspot.com/");
 
         return rootView;
     }
 
-    private class InnerWebViewClient extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals("https://relevantxkcd.appspot.com/")) {
-                return false;
-            }
-
-            Log.d(TAG, "Taken url from web: " + url);
-            // Show the loaded url into the web view
-            webView.loadUrl(url);
-
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-
-            /*webView.loadUrl("javascript:(function() { " +
-                    "document.getElementsByTagName('comic')[0].style.display='none';" +
-                    "})()");*/
-        }
-    }
 }
 
 
